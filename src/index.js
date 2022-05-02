e0cbc0f4dd9598e0c4aa03ba8a2917f701a87dab
Mawusee FOLI-AWLI <mawusee.foli@gmail.com>
2022-05-02 23:51:28 +0000

    Adding a header to the page

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
-import Counter from './components/Counter'
-import counter from './reducers'
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import Counter from "./components/Counter";
+import counter from "./reducers";
 
-const store = createStore(counter)
-const rootEl = document.getElementById('root')
+const store = createStore(counter);
+const rootEl = document.getElementById("root");
 
-const render = () => ReactDOM.render(
+const display = (
   <>
-  <h1>Welcome to our counter to prod</h1>
-  <Counter
-    value={store.getState()}
-    onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
-    onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
-  />,
-  rootEl
+    <h1>Welcome to our counter to prod</h1>
+    <Counter
+      value={store.getState()}
+      onIncrement={() => store.dispatch({ type: "INCREMENT" })}
+      onDecrement={() => store.dispatch({ type: "DECREMENT" })}
+    />
   </>
-)
+);
 
-render()
-store.subscribe(render)
+const render = () => ReactDOM.render(display, rootEl);
+
+render();
+store.subscribe(render);
